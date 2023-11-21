acc7e58f60eb4988a86a58a378eec02101aa28af
Brunetteee <147651656+Brunetteee@users.noreply.github.com>
2023-11-21 13:13:25 +0100

    Add files via upload

---
+++ lesson1/hw1.js
@@ -0,0 +1,52 @@
+console.log('-------1.1---------');
+
+let s1 = "'Hello'" ;
+console.log(s1);
+let s2 = "'owu'";
+console.log(s2);
+let s3 = "'com'" ;
+console.log(s3);
+let s4 = "'ua'";
+console.log(s4);
+let n1 = 1;
+console.log(n1);
+let n2 = 10;
+console.log(n2);
+let n3 = -990;
+console.log(n3);
+let n4 = 123;
+console.log(n4);
+let n5 = 3.14;
+console.log(n5);
+let n6 = 2.7;
+console.log(n6);
+let n7 = 16;
+console.log(n7);
+
+console.log('-------1.2---------');
+
+let firstName = 'Ryan';
+let middleName = 'Reynolds';
+let lastName = 'Aleksandrovich';
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+console.log('-------1.3---------');
+
+let a=100; let b='100'; let c=true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log('--------1.4-----------')
+
+let firstName1 = prompt("Введіть ваше Ім'я");
+let middleName2 = prompt("Введіть ваше Прізвища");
+let age = prompt("Введіть ваш вік");
+console.log(firstName1)
+console.log(middleName2)
+console.log(age)
+
+
+
+
